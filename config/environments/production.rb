4ec8ba81a82e45933a86dd0de53a254c40debdc6
Scott Goci <scottjg@gmail.com>
2014-04-01 13:02:03 -0400

    Changed production host to be heroku until we move to the subdomain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,10 +65,10 @@ Kandan::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => "wwww.alfajango.com" }
+  config.action_mailer.default_url_options = { :host => "aj-kandan.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  
+
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 25,
